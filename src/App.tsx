e38dff03c0696559cfd67de23e09fdde5d96838a
Дмитрий <clearsky2@mail.ru>
2025-10-04 20:40:26 +0500

    Разделение на страницу пиццы и главную. Создание маршрутов

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,18 @@
 import React, { FC, useEffect, useState } from 'react';
-import AddPizzaForm from './components/AddPizzaForm';
-import DisplayPizzas from './components/DisplayPizzas';
-import Pizza from './models/Pizza';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import PizzaPage from './pages/PizzaPage';
 import './App.css';
 
 
 const App: FC = () => {
-  const [pizzasList, setPizzasList] = useState<Pizza[]>([]);
-
-  const addPizza = (newPizza: Pizza) => {
-    const newPizzasList = [...pizzasList, newPizza];
-    setPizzasList(newPizzasList);
-    localStorage.setItem('pizzasState', JSON.stringify(newPizzasList));
-  }
-
-  const updatePizza = (newPizza: Pizza) => {
-    const newPizzasList = pizzasList.map(pizza => (pizza.id === newPizza.id ? newPizza : pizza));
-    setPizzasList(newPizzasList);
-    localStorage.setItem('pizzasState', JSON.stringify(newPizzasList));
-  }
-
-  const deletePizza = (id: number) => {
-    const newPizzasList = pizzasList.filter(pizza => pizza.id !== id);
-    setPizzasList(newPizzasList);
-    localStorage.setItem('pizzasState', JSON.stringify(newPizzasList));
-  }
-
-  useEffect(() => {
-    const pizzasState = localStorage.getItem('pizzasState');
-    if (pizzasState) {
-      setPizzasList(JSON.parse(pizzasState));
-    }
-  }, []);
-
-  console.log('pizzasList >> ', pizzasList);
-
   return (
     <div className="App">
       <div className='wrap'>
-        <span className='heading'>Наша пиццерия</span>
-        <AddPizzaForm
-          addPizza={addPizza}
-        />
-        <DisplayPizzas 
-          pizzasList={pizzasList}
-          updatePizza={updatePizza}
-          deletePizza={deletePizza}
-        />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/pizza/:id" element={<PizzaPage />} />
+        </Routes>
       </div>
     </div>
   );
