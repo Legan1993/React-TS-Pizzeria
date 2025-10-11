1e1414c0a98fa25e2b3a6618b1bff788212de18b
Дмитрий <clearsky2@mail.ru>
2025-10-11 12:23:57 +0500

    Пагинация

--- src/components/DisplayPizzas.tsx
+++ src/components/DisplayPizzas.tsx
@@ -1,6 +1,7 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import Pizza from '../models/Pizza';
 import SinglePizza from './SinglePizza';
+import Pagination from './Pagination';
 
 interface DisplayPizzasProps {
     pizzasList: Pizza[];
@@ -9,13 +10,36 @@ interface DisplayPizzasProps {
 }
 
 const DisplayPizzas: FC<DisplayPizzasProps> = ({ pizzasList, updatePizza, deletePizza }) => {
-    return (
-        <div className="container">
-            {pizzasList.map((pizza) => {
-                return <SinglePizza key={pizza.id} pizza={pizza} updatePizza={updatePizza} deletePizza={deletePizza} />;
-            })}
-        </div>
-    )
-}
+
+  const [curPage, setCurPage] = useState<number>(1);
+  const itemsPerPage = 2;
+  const skip = curPage * itemsPerPage - itemsPerPage;
+  const pizzasPaginated = pizzasList.slice(skip, curPage * itemsPerPage);
+
+  const handleNext = () => {
+    setCurPage(curPage + 1);
+  }
+  const handlePrev = () => {
+    setCurPage(curPage - 1);
+  }
+
+  return (
+    <>
+      <div className="container">
+        {pizzasPaginated?.map((pizza) => (
+          <SinglePizza key={pizza.id} pizza={pizza} updatePizza={updatePizza} deletePizza={deletePizza} />
+        ))}
+      </div>
+      
+      <Pagination 
+        curPage={curPage}
+        itemsPerPage={itemsPerPage}
+        handleNext={handleNext}
+        handlePrev={handlePrev}
+        pizzasList={pizzasList}
+      />
+    </>
+  );
+};
 
 export default DisplayPizzas;
